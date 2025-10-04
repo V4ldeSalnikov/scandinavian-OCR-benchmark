b398cf08f93f84724aeb1ddcf10997687ae1a39d
Vladimir Salnikov <v4ldesalnikov@gmail.com>
2025-10-04 19:17:50 +0200

    added batch call method and dataclass

--- models/model_interface.py
+++ models/model_interface.py
@@ -1,18 +1,23 @@
 from abc import ABC, abstractmethod
 from pydantic import BaseModel, Field
 from PIL import Image
+from dataclasses import dataclass
 
+@dataclass
 class OCRInput:
-    images : list[Image.Image]
-class OCROutput:
-    text : list[str]
+    image : Image.Image
 
-class OCRModel(BaseModel):
+@dataclass
+class OCROutput:
+    text : str
+
+class OCRModel(ABC):
     id : str
     supported_tasks : list[str]
 
     @abstractmethod
     def __call__(self, inputs : OCRInput) -> OCROutput:
         ...
-
+    def batch_call(self, inputs: list[OCRInput]) -> list[OCROutput]:
+        return [self(inpt) for inpt in inputs]
 
